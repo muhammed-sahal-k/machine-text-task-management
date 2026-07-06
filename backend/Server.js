4f9acbafde9c3cafb1a5fb060f89cc00bfc33dbc
muhammed-sahal-k <sahalkmohammed95@gmail.com>
2026-07-06 12:13:50 +0530

    Initial full stack task management project

--- backend/Server.js
+++ backend/Server.js
@@ -1,40 +1,5 @@
 
 
-// import cors from "cors";
-// import dotenv from "dotenv";
-// import express from "express";
-// import path from "path";
-// import connection from "./Connection.js";
-// import Router from "./Router.js";
-// dotenv.config();
-
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json({ limit: "50mb" }));
-
-// // ✅ serve uploads (proof files)
-// app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
-
-// app.use("/api", Router);
-
-// connection()
-//   .then(() => {
-//     app.listen(process.env.PORT, () => {
-//       console.log(`✅ Server running at http://localhost:${process.env.PORT}`);
-//     });
-//   })
-//   .catch((err) => console.log(err));
-
-
-
-
-
-
-
-
-
-
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
